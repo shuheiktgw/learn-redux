8336ebd97a6dddfe7b074cafcaf7a8083c0a742c
shuheiktgw <shuhei.kitagawa@gmail.com>
2016-11-03 10:22:37 +0900

    refactor file to create separate files for actions, reducers, store

--- app/redux-example.jsx
+++ app/redux-example.jsx
@@ -1,139 +1,9 @@
 var redux = require('redux');
-var axios = require('axios');
 
 console.log('Starting redux example');
 
-var nameReducer = (state = 'Anonymous', action) => {
-  switch (action.type) {
-    case 'CHANGE_NAME':
-      return action.name;
-    default:
-      return state
-  }
-};
-
-var changeName = (name) => {
-  return {
-    type: 'CHANGE_NAME',
-    name
-  }
-};
-
-var nextHobbyId = 1;
-var hobbiesReducer = (state = [], action) => {
-  switch (action.type) {
-    case 'ADD_HOBBY':
-      return [
-        ...state,
-        {
-          id: nextHobbyId++,
-          hobby: action.hobby
-        }
-      ];
-    case 'REMOVE_HOBBY':
-      return state.filter((hobby) => hobby.id !== action.id);
-    default:
-      return state
-  }
-};
-
-var addHobby = (hobby) => {
-  return {
-    type: 'ADD_HOBBY',
-    hobby
-  }
-};
-
-var removeHobby = (id) => {
-  return {
-    type: 'REMOVE_HOBBY',
-    id
-  }
-};
-
-var nextMovieId = 1;
-var moviesReducer = (state = [], action) => {
-  switch (action.type) {
-    case 'ADD_MOVIE':
-      return [
-        ...state,
-        {
-          id: nextMovieId++,
-          title: action.title,
-          genre: action.genre
-        }
-      ];
-    case 'REMOVE_MOVIE':
-      return state.filter((movie) => movie.id !== action.id)
-    default:
-      return state
-  }
-};
-
-var addMovie = (title, genre) => {
-  return {
-    type: 'ADD_MOVIE',
-    title,
-    genre
-  }
-};
-
-var removeMovie = (id) => {
-  return {
-    type: 'REMOVE_HOBBY',
-    id
-  }
-};
-
-var mapReducer = (state = {isFetching: false, url: undefined}, action) => {
-  switch (action.type) {
-    case 'START_LOCATION_FETCH':
-      return {
-        isFetching: true,
-        url: undefined
-      };
-    case 'COMPLETE_LOCATION_FETCH':
-      return {
-        isFetching: false,
-        url: action.url
-      };
-    default:
-      return state
-  }
-};
-
-var startLocationFetch = () => {
-  return {
-    type: 'START_LOCATION_FETCH',
-  }
-};
-
-var completeLocationFetch = (url) => {
-  return {
-    type: 'COMPLETE_LOCATION_FETCH',
-    url
-  }
-};
-
-var fetchLocation = () => {
-  store.dispatch(startLocationFetch());
-
-  axios.get('http://ipinfo.io').then((res) => {
-    var loc = res.data.loc
-    var baseUrl = 'http://maps.google.com?q='
-
-    store.dispatch(completeLocationFetch(baseUrl + loc))
-  })
-};
-
-var reducer = redux.combineReducers({
-  name: nameReducer,
-  hobbies: hobbiesReducer,
-  movies: moviesReducer,
-  map: mapReducer
-});
-
-var store = redux.createStore(reducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+var actions = require('./actions/index');
+var store = require('./store/configureStore').configure();
 
 var unsubscribe = store.subscribe(() => {
   var state = store.getState();
@@ -147,14 +17,14 @@ var unsubscribe = store.subscribe(() => {
   }
 });
 
-fetchLocation()
+store.dispatch(actions.fetchLocation())
 
-store.dispatch(changeName('Shuhei'));
-store.dispatch(addHobby("Running"));
-store.dispatch(addHobby("Walking"));
-store.dispatch(removeHobby(1));
-store.dispatch(changeName('Emily'));
-store.dispatch(addMovie("Star Wars", "Action"));
-store.dispatch(addMovie("The big bang theory", "Action"));
-store.dispatch(removeMovie(2));
+store.dispatch(actions.changeName('Shuhei'));
+store.dispatch(actions.addHobby("Running"));
+store.dispatch(actions.addHobby("Walking"));
+store.dispatch(actions.removeHobby(1));
+store.dispatch(actions.changeName('Emily'));
+store.dispatch(actions.addMovie("Star Wars", "Action"));
+store.dispatch(actions.addMovie("The big bang theory", "Action"));
+store.dispatch(actions.removeMovie(2));
 
